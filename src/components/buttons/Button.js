4560e1a2b3f5321ca40d6162a5be6c386d15f6c1
EduardoLaranjo <eduardolaranjo@gmail.com>
2020-05-25 16:39:26 +0100

    button

--- src/components/buttons/Button.js
+++ src/components/buttons/Button.js
@@ -3,9 +3,11 @@ import React, {Component} from 'react';
 class Button extends Component {
   render() {
     return (
-        <div>
-          <button>Ola Mundo</button>
-        </div>
+      <div>
+        <button>Ola Mundo</button>
+      </div>
     );
   }
 }
+
+export default Button;
