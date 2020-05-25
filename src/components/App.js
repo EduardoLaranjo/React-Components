ba9abbd53a0df68a8a29e4f2c01eafacd23a7e21
EduardoLaranjo <eduardolaranjo@gmail.com>
2020-05-25 16:41:33 +0100

    add routes in App component
    #Dropdown
    #Button
    #Drop
    #Input

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,32 @@
 import React, {Component} from 'react';
 import Dropdown from './dropdowns/Dropdown';
+import {BrowserRouter, Route} from 'react-router-dom';
+import Button from './buttons/Button';
+import InputAutomatic from './inputs/InputAutomatic';
+import Sidebar from './Sidebar';
+import DropClick from './drag&drops/DropClick';
 
 class App extends Component {
 
   render() {
     return (
-        <div>
-          <Dropdown/>
-        </div>
+      <div style={{display: 'flex'}}>
+        <BrowserRouter>
+          <Sidebar/>
+          <Route exact path={'/dropdown'}>
+            <Dropdown/>
+          </Route>
+          <Route exact path={'/button'}>
+            <Button/>
+          </Route>
+          <Route exact path={'/drop'}>
+            <DropClick/>
+          </Route>
+          <Route exact path={'/input'}>
+            <InputAutomatic/>
+          </Route>
+        </BrowserRouter>
+      </div>
     );
   }
 }
